3dba0dd4a72f4df9f46153145ef54bc5977f554b
Wechuli <wechulipaul@yahoo.com>
2020-12-21 10:33:12 +0300

    added test

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace actiondotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Paul","Wechuli");
+            Console.WriteLine($"{p.FirstName},{p.LastName}");
+        }
+    }
+
+    class Person{
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+
+        public Person(string firstName,string lastName){
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
